9b765db4c6cb803f8d0119d6a5c26575a4b49ac0
Bane Radosavljević <r_bane@hotmail.com>
2020-03-19 14:51:50 +0100

    Episode 7

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -6,39 +6,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-     /** @test */
-     public function guests_cannot_create_projects()
-     {
-         // $this->withoutExceptionHandling();
- 
-         $attributes = factory('App\Project')->raw();
- 
-         $this->post('/projects', $attributes)->assertRedirect('login');
-     }
+        /** @test */
+    public function guests_cannot_manage_projects()
+    {
+        // $this->withoutExceptionHandling();
 
-     /** @test */
-     public function guests_cannot_view_projects()
-     {
-         // $this->withoutExceptionHandling();
- 
-         $attributes = factory('App\Project')->raw();
- 
-         $this->get('/projects')->assertRedirect('login');
-     }
+        $project = factory('App\Project')->create();
 
-     /** @test */
-     public function guests_cannot_view_a_single_project()
-     {
-         // $this->withoutExceptionHandling();
- 
-         $project = factory('App\Project')->create();
- 
-         $this->get($project->path())->assertRedirect('login');
-     }
+        $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
+        $this->get($project->path())->assertRedirect('login');
+    }
 
     /** @test */
     public function a_user_can_create_a_project()
@@ -46,6 +29,8 @@ class ProjectsTest extends TestCase
         $this->withoutExceptionHandling();
 
         $this->actingAs(factory('App\User')->create());
+
+        $this->get('/projects/create')->assertStatus(200);
 
         $attributes = [
             'title' => $this->faker->sentence,
@@ -60,13 +45,13 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_their_project()    
+    public function a_user_can_view_their_project()
     {
         $this->be(factory('App\User')->create());
         $this->withoutExceptionHandling();
 
         $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
-        
+
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
@@ -80,7 +65,6 @@ class ProjectsTest extends TestCase
 
         $project = factory('App\Project')->create();
         $this->get($project->path())->assertStatus(403);
-
     }
 
     /** @test */
@@ -102,6 +86,4 @@ class ProjectsTest extends TestCase
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
-
-   
 }
