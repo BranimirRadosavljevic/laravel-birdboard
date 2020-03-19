cb3276c508d45ef51e36a76810dfa7d320dacc1f
Bane Radosavljević <r_bane@hotmail.com>
2020-03-19 15:51:37 +0100

    Episode 8

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.css">
-    <title>Document</title>
-</head>
-<body>
-    <form method="POST" action="/projects" class="container" style="padding-top:40px">
-        @csrf
-        <h1 class="heading is-1">Create a project</h1>
-        <div class="field">
-            <label class="label" for="title">Title</label>
-            <div class="control">
-                <input type="text" name="title" class="input" placeholder="Title">
-            </div>
+@extends('layouts.app')
+@section('content')
+    
+<form method="POST" action="/projects">
+    @csrf
+    <h1 class="heading is-1">Create a project</h1>
+    <div class="field">
+        <label class="label" for="title">Title</label>
+        <div class="control">
+            <input type="text" name="title" class="input" placeholder="Title">
         </div>
-        <div class="field">
-            <label for="description" class="label">Description</label>
-            <div class="control">
-                <textarea name="description" class="textarea" cols="30" rows="10"></textarea>
-            </div>
+    </div>
+    <div class="field">
+        <label for="description" class="label">Description</label>
+        <div class="control">
+            <textarea name="description" class="textarea" cols="30" rows="10"></textarea>
         </div>
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="button is-link">Create Project</button>
-            </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <button type="submit" class="button is-link">Create Project</button>
+            <a href="/projects">Cancel</a>
         </div>
-    </form>
-</body>
-</html>
+    </div>
+</form>
+@endsection
