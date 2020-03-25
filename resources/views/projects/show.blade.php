d6820d1cb1680e0d4d6233e0070ede32f508fd1f
Bane Radosavljević <r_bane@hotmail.com>
2020-03-25 21:44:40 +0100

    Episode 32

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,9 +6,22 @@
         <p class="text-grey text-sm font-normal">
             <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a>/ {{$project->title}}
         </p>
-        <a href="{{$project->path().'/edit'}}" class="button">Edit Project</a>    
+
+        <div class="flex items-center">
+            @foreach ($project->members as $member)
+
+                <img src="{{ gravatar_url($member->email)}}" alt="{{ $member->name }}'s avatar'" class="rounded-full w-8 mr-2">
+           
+            @endforeach
+
+                <img src="{{ gravatar_url($project->owner->email)}}" alt="{{ $project->owner->name }}'s avatar'" class="rounded-full w-8 mr-2">
+
+
+            <a href="{{$project->path().'/edit'}}" class="button ml-4">Edit Project</a>
+        </div>    
     </div>    
 </header>
+
 <main>
     <div class="lg:flex -mx-3">
         <div class="lg:w-3/4 px-3 mb-6">
