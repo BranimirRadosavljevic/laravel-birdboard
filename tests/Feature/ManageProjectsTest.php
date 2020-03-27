c91cdcbf5c90e8e79fa2f569c4b383649a1ed5fe
Bane Radosavljević <r_bane@hotmail.com>
2020-03-27 13:06:15 +0100

    Episode 35

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -33,19 +33,11 @@ class ManageProjectsTest extends TestCase
 
         $this->get('/projects/create')->assertStatus(200);
 
-        $attributes = [
-            'title' => $this->faker->sentence,
-            'description' => $this->faker->sentence,
-            'notes' => 'General notes here.'
-        ];
+        $attributes = factory(Project::class)->raw();
 
-        $response = $this->post('/projects', $attributes);
+        $response = $this->followingRedirects()->post('/projects', $attributes);
 
-        $project = Project::where($attributes)->first();
-
-        $response->assertRedirect($project->path());
-
-        $this->get($project->path())
+        $response
             ->assertSee($attributes['title'])
             ->assertSee($attributes['description'])
             ->assertSee($attributes['notes']);
@@ -57,11 +49,10 @@ class ManageProjectsTest extends TestCase
         $user = $this->signIn();
 
         $project = ProjectFactory::create();
-        
+
         $project->invite($user);
 
         $this->get('/projects')->assertSee($project->title);
-
     }
 
     /** @test */
@@ -72,10 +63,13 @@ class ManageProjectsTest extends TestCase
         $this->delete($project->path())
             ->assertRedirect('/login');
 
-        $this->signIn();
+        $user = $this->signIn();
 
-        $this->delete($project->path())
-            ->assertStatus(403);
+        $this->delete($project->path())->assertStatus(403);
+
+        $project->invite($user);
+
+        $this->actingAs($user)->delete($project->path())->assertStatus(403);
     }
 
     /** @test */
