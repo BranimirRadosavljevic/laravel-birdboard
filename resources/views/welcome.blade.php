dd2161a6689f0049e5ff14432dc992a71e59e7aa
Bane Radosavljević <r_bane@hotmail.com>
2020-03-29 18:27:56 +0200

    Completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #47cdff;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -70,7 +70,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}" class="">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
@@ -80,19 +80,13 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="title mb-8">
+                    Birdboard
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="links">                    
+                    <a href="https://laracasts.com" class="text-accent">WATCH THE LARACASTS</a>
+                    
                 </div>
             </div>
         </div>
